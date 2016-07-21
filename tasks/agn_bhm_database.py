51da07d1ae33c33b0f324d12955ea72dbdb62492
lzkelley <lkelley@cfa.harvard.edu>
2016-07-21 15:41:18 -0400

    ENH: finished processing subpages, and getting all sources, for 'agn_bhm_database'.

--- tasks/agn_bhm_database.py
+++ tasks/agn_bhm_database.py
@@ -9,12 +9,15 @@ import csv
 import re
 import bs4
 from bs4 import BeautifulSoup
+import numpy as np
 import astropy as ap
 import astropy.constants
+import sys
 
 from astrocats.catalog.utils import pbar
-from astrocats.catalog.quantity import QUANTITY
 from astrocats.catalog.photometry import PHOTOMETRY
+from astrocats.catalog.quantity import QUANTITY
+from astrocats.catalog.source import SOURCE, Source
 
 from astrocats.blackholes.blackhole import BLACKHOLE
 
@@ -23,9 +26,13 @@ SOURCE_URL = "http://adsabs.harvard.edu/abs/2015PASP..127...67B"
 DATA_URL = "http://www.astro.gsu.edu/AGNmass/"
 DATA_SUBPAGE_URL = "http://www.astro.gsu.edu/AGNmass/details.php?varname={}"
 
+DESC_AGN_LUM = "Spectroscopic, monochromatic luminosities at 5100 angstrom."
+
 
 def do_agn_bhm_database(catalog):
     """Load data from the 'AGN Blackhole Mass Database': 2015PASP..127...67B.
+
+    FIX: Add archiving flags!
     """
     log = catalog.log
     log.debug("do_agn_bhm_database()")
@@ -87,12 +94,12 @@ def _add_entry_for_data_line(catalog, line, varname, mass_scale_factor):
 
     Columns:
     -------
-    [ ] 00 - object name
-    [ ] 01 - mass (+###/-###)   [ log(Mbh/Msol) ]
-    [ ] 02 - RA (hh:mm:ss.s)
-    [ ] 03 - Dec (dd:mm:ss)
-    [ ] 04 - redshift z
-    [ ] 05 - Alternate names    [whitespace delimiated strings]
+    00 - object name
+    01 - mass (+###/-###)   [ log(Mbh/Msol) ]
+    02 - RA (hh:mm:ss.s)
+    03 - Dec (dd:mm:ss)
+    04 - redshift z
+    05 - Alternate names    [whitespace delimiated strings]
 
     Sample Entry:
     ------------
@@ -116,6 +123,26 @@ def _add_entry_for_data_line(catalog, line, varname, mass_scale_factor):
     source = catalog.entries[name].add_source(
         url=SOURCE_URL, bibcode=SOURCE_BIBCODE, secondary=True)
 
+    # Get data from blackhole-specific subpage
+    # ----------------------------------------
+    all_sources = [source]
+    source_names, source_urls, source_codes = _load_blackhole_subpage_data(
+        catalog, name, varname, source)
+    # Warn on failure, but assume the entry is still okay.
+    if not len(source_names):
+        _warn(catalog, "Failed to load subpage for varname '{}'.".format(varname), line, name)
+    # Add additional sources from sub-page
+    else:
+        for sn, su, sb in zip(source_names, source_urls, source_codes):
+            src_kwargs = {SOURCE.NAME: sn, SOURCE.URL: su}
+            if sb is not None:
+                src_kwargs[SOURCE.BIBCODE] = sb
+            src = catalog.entries[name].add_source(**src_kwargs)
+            all_sources.append(src)
+
+    # Join source-aliases into single string
+    all_sources = ",".join(src for src in all_sources)
+
     # Mass and Uncertainties
     # ----------------------
     # This is either no entry '...' or something like:
@@ -132,12 +159,10 @@ def _add_entry_for_data_line(catalog, line, varname, mass_scale_factor):
             bh_mass, err = [re.sub(r'[ ()+-]', r'', mm) for mm in bh_mass]
             # Split into higher and lower errors
             err_hi, err_lo = err.split('/')
-
-            # FIX: add additional sources from the sub-pages
-            use_sources = source
             quant_kwargs = {QUANTITY.U_VALUE: 'log(Msol)', QUANTITY.DESC: mass_desc,
                             QUANTITY.E_LOWER_VALUE: err_lo, QUANTITY.E_UPPER_VALUE: err_hi}
-            catalog.entries[name].add_quantity(BLACKHOLE.MASS, bh_mass, use_sources, **quant_kwargs)
+            catalog.entries[name].add_quantity(
+                BLACKHOLE.MASS, bh_mass, all_sources, **quant_kwargs)
 
     # RA/Dec
     # ------
@@ -168,18 +193,17 @@ def _add_entry_for_data_line(catalog, line, varname, mass_scale_factor):
         if len(cells):
             _warn(catalog, "`cells` still not empty! '{}'".format(cells), line, name)
 
-    # Get data from blackhole-specific subpage
-    # ----------------------------------------
-    retval = _load_blackhole_subpage_data(catalog, name, varname, source)
-    # Warn on failure, but assume the entry is still okay.
-    if not retval:
-        _warn(catalog, "Failed to load subpage for varname '{}'.".format(varname), line, name)
-
     return name
 
 
 def _load_blackhole_subpage_data(catalog, name, varname, source):
     """Load data from this entry's dedicated subpage.
+
+    Returns
+    -------
+    source_names : list of str
+    source_urls : list of str
+    source_codes : list of (str or 'None')
     """
     # Construct URL and load HTML data
     data_url = DATA_SUBPAGE_URL.format(varname)
@@ -189,19 +213,98 @@ def _load_blackhole_subpage_data(catalog, name, varname, source):
     if not html:
         catalog.log.error("{} Failed to load data from '{}'.".format(
             task_name, DATA_URL))
-        return False
+        return [], [], []
 
     # Extract the 'activity' of the BH
     # --------------------------------
     activ = re.search("<b>Activity:</b>(.*)</td><td>", html).groups()
-    print(activ)
     if len(activ) == 1:
-        catalog.entries[name].add_quantity(BLACKHOLE.ACTIVITY, bh_mass, use_sources, **quant_kwargs)
+        activ = activ[0].strip()
+        if len(activ):
+            catalog.entries[name].add_quantity(BLACKHOLE.ACTIVITY, activ, source)
+
+    # Extract Luminosities and citations from the table
+    # -------------------------------------------------
+    # WHAT LUMINOSITY IS THIS?!?!?!  AGN,5100??
+
+    soup = BeautifulSoup(html, 'html5lib')
+    # The whole table is nested in a `<table class="body">`
+    full_table = soup.find('table', attrs={'class': 'body'})
+    source_names = []
+    source_urls = []
+    source_codes = []
+    # Go through each line of table
+    for table_line in full_table.find_all('tr'):
+        childs = list(table_line.children)
+        # Valid, normal lines have 11 cells in them (i.e. besides header and filler)
+        if len(childs) == 11:
+            for ii in [3, 8]:
+                src_name, src_url, src_code = _source_url_name_from_cell(childs[ii])
+                if src_name is not None and src_url is not None:
+                    source_names.append(src_name)
+                    source_urls.append(src_url)
+                    source_codes.append(src_code)
+            # Get AGN Luminosity
+            lum_cell = childs[9].text.strip().split('+/-')
+            # Blank lines have ellipses, valid lines have '+/-' uncertainty
+            if len(lum_cell) == 2:
+                lum_cell = [re.sub(r'[ ()]', r'', lc) for lc in lum_cell]
+                val, err = lum_cell
+                # The '10'th cell has the luminosity citation
+                src_name, src_url, src_code = _source_url_name_from_cell(childs[10])
+                if src_name is not None and src_url is not None:
+                    # Add source for luminosity
+                    src_kwargs = {SOURCE.NAME: src_name, SOURCE.URL: src_url}
+                    if src_code is not None:
+                        src_kwargs[SOURCE.BIBCODE] = src_code
+                    lum_src = catalog.entries[name].add_source(**src_kwargs)
+                    # List source as both primary and secondary sources
+                    lum_src = ",".join([source, lum_src])
+                    photo_kwargs = {
+                        PHOTOMETRY.LUMINOSITY: val, PHOTOMETRY.SOURCE: lum_src,
+                        PHOTOMETRY.HOST: True, PHOTOMETRY.E_LUMINOSITY: err,
+                        PHOTOMETRY.U_LUMINOSITY: 'log(erg/s)', PHOTOMETRY.DESC: DESC_AGN_LUM,
+                        PHOTOMETRY.WAVELENGTH: '5100', PHOTOMETRY.U_WAVELENGTH: 'angrstrom'
+                    }
+                    catalog.entries[name].add_photometry(**photo_kwargs)
+
+            elif childs[9].text.strip() != '...':
+                raise ValueError("Unexpected value in `childs[9]` = '{}'.".format(chils[9]))
+
+    # Cut down to unique sources
+    source_urls, inds = np.unique(source_urls, return_index=True)
+    source_names = [source_names[ii] for ii in inds]
+    source_codes = [source_codes[ii] for ii in inds]
+
+    return source_names, source_urls, source_codes
 
 
-    # print(html)
+def _source_url_name_from_cell(cell):
+    """Given a table cell (`bs4.element.NavigableString`) try to get citation name and url.
 
-    return True
+    Returns 'None' values if either is missing or empty.
+
+    Returns
+    -------
+    name : str
+    url : str
+    bibcode : str
+
+    """
+    # soup = BeautifulSoup(html, 'html5lib')
+    source_cell = cell.find('a', href=True)
+    try:
+        name = source_cell.text.strip()
+        url = source_cell['href'].strip()
+    except AttributeError:
+        return None, None, None
+
+    if not len(name) or not len(url):
+        return None, None, None
+
+    bibcode = Source.bibcode_from_url(url)
+
+    return name, url, bibcode
 
 
 def _warn(catalog, msg, line=None, name=None):
