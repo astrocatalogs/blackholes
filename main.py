7625a00f5df28ef52d5da69bbf91f4b968cd9939
lzkelley <lkelley@cfa.harvard.edu>
2017-05-18 13:38:32 -0400

    MAINT: minor logging changes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 def main(args, clargs, log):
+    log.debug("blackholes.main.main()")
     from .blackholecatalog import BlackholeCatalog
     from astrocats.catalog.argshandler import ArgsHandler
 
@@ -13,12 +14,15 @@ def main(args, clargs, log):
     args = args_handler.load_args(args=args, clargs=clargs)
     # Returns 'None' if no subcommand is given
     if args is None:
+        log.warning("No `args` given.")
         return
 
     # Create the appropriate type of catalog
+    log.info("Creating `BlackholeCatalog`")
     catalog = BlackholeCatalog(args, log)
 
     # Run the subcommand given in `args`
+    log.info("Running subcommand")
     args_handler.run_subcommand(args, catalog)
 
     return
