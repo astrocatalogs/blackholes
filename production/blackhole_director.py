25ba2181e2f72c553df07d3a6492661de4fdac00
lzkelley <lkelley@cfa.harvard.edu>
2017-05-30 17:30:32 -0400

    ENH: subclass 'HTML_Pro' to add customization.

--- production/blackhole_director.py
+++ production/blackhole_director.py
@@ -1,8 +1,10 @@
 """Blackhole Catalog: `Director` subclass
 """
 
-from astrocats.catalog.production import director
+from astrocats.catalog.production import director, html_pro
 from astrocats.catalog.utils import dict_to_pretty_string
+from astrocats.catalog import QUANTITY
+from .. blackhole import BLACKHOLE
 
 
 class Blackhole_Director(director.Director):
@@ -11,6 +13,22 @@ class Blackhole_Director(director.Director):
                         'agn_activity', 'tasks']
     # _DEL_QUANTITY_KEYS = ['description']
 
-    def update(self, fname, event_name, event_data):
-        # print(dict_to_pretty_string(event_data))
-        super().update(fname, event_name, event_data)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.HTML_Pro = BH_HTML_Pro
+        return
+
+    # def update(self, fname, event_name, event_data):
+    #     # print(dict_to_pretty_string(event_data))
+    #     super().update(fname, event_name, event_data)
+
+
+class BH_HTML_Pro(html_pro.HTML_Pro):
+
+    def _meta_data_entry_kind(self, key, row):
+        """Retrieve an additional 'kind' parameter to add to a Meta-Data value cell.
+        """
+        if key == BLACKHOLE.MASS and QUANTITY.KIND in row:
+            return row[QUANTITY.KIND]
+
+        return
