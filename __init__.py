85ed553094cd68806d54e4bfb3c6fe9d5c594740
lzkelley <lkelley@cfa.harvard.edu>
2018-08-08 14:44:01 -0400

    Load path to schema in module

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 """The Blackholes AstroCatalog.
 """
 # flake8: noqa  --- ignore imported but unused flake8 warnings
+import os
 
 from . import main
-from . import production
+# from . import production
+
+_PATH_BLACKHOLES = os.path.join(os.path.dirname(__file__), "")
+PATH_SCHEMA = os.path.join(_PATH_BLACKHOLES, "schema", "")
