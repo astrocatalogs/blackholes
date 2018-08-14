22dda70f930c68a929e3067bb98f190dd0e6e20a
lzkelley <lkelley@cfa.harvard.edu>
2018-08-14 12:01:35 -0400

    Add additional 'catalog_info' to module (schema path)

--- __init__.py
+++ __init__.py
@@ -2,10 +2,16 @@
 """
 import os
 
-catalog_class = {
-    "name": "BlackholeCatalog",
-    "file": "blackholecatalog",
-    "import_path": "blackholes."
+_ROOT = os.path.join(os.path.dirname(__file__), "")
+
+catalog_info = {
+    "catalog_name": __name__,
+    "catalog_class": {
+        "name": "BlackholeCatalog",
+        "file": "blackholecatalog",
+        "path": "blackholes."
+    },
+    "schema_path": os.path.join(_ROOT, "schema", "")
 }
 
 
@@ -14,9 +20,13 @@ import astrocats
 
 class BH_Paths(astrocats.Paths):
 
-    ROOT = os.path.join(os.path.dirname(__file__), "")
+    ROOT = _ROOT
     NAME = __name__
     FILE = __file__
+
+    def __init__(self):
+        super(BH_Paths, self).__init__()
+        return
 
 
 PATHS = BH_Paths()
